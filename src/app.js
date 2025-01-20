9a2e949999cccd22f34a7a1e6283ba9516819072
Sneha <bhardwajsneha111@gmail.com>
2025-01-20 11:34:13 +0530

    exploring http methods

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const express= require("express");
 const app=express();
-app.use("/",(req, res)=>{
-    res.send("hell ");
-});
-app.use("/test", (req, res)=>{
-    res.send("hello test");
-});
+app.get("/hello", (req, res)=>{
+    res.send({firstName:"sneha", LastName: "Sharma"});
+})
+app.post("/hello", (req, res)=>{
+      res.send("data saved successfully");
+})
 app.listen(3000, ()=>{console.log("app is running on port 3000")});
